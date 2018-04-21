957430dbd001b19116ccc5cc96c8f472c15ce855
Samparkyn <samparkyn101@gmail.com>
2018-04-21 19:53:09 +0200

    Header styles & add Vision font

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import './header.css'
+
 export class Header extends Component {
   state = {
 
@@ -9,10 +11,17 @@ export class Header extends Component {
   render() {
 
     return (
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/parks">Parks</Link>
-        <Link to="/coasters">Coasters</Link>
+      <div className="header">
+        <div className="header__wrapper">
+          <div className="header__container-left">
+            <Link className="header__link" to="/">Home</Link>
+          </div>
+          <div className="header__container-right">
+            <Link className="header__link" to="/parks">Parks</Link>
+            <Link className="header__link" to="/coasters">Coasters</Link>
+            <Link className="header__link" to="/login">Login</Link>
+          </div>
+        </div>
       </div>
     )
 
