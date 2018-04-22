60cbcb28695a9da95cdd84b9e9d6318a3b3ea99c
Samparkyn <samparkyn101@gmail.com>
2018-04-22 12:12:54 +0200

    Add 'Launch' button on home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,7 +5,10 @@ export const Home = () => {
   return (
     <div className="home">
       <div className="home__background" />
-      <p>Explore and record your favourite parks and coasters </p>
+      <p className="intro-text">Explore and record your favourite parks and coasters </p>
+      <div className="btn__container">
+        <button className="btn">Launch me!</button>
+      </div>
     </div>
   )
 }
