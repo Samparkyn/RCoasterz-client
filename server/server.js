b3a046422114af2ae551e21ed379c9ae42be721d
Samparkyn <samparkyn101@gmail.com>
2018-06-23 11:17:37 +0200

    Add server.js

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express    = require('express');
+const bodyParser = require('body-parser');
+const cors       = require('cors')
+const app        = express();
+const port       = 8000;
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const data = require('../mock-data.json')
+const parsedData = JSON.stringify(data)
+
+app.listen(port, () => {
+  console.log("Ready on port:", port);
+});
+
+app.get('/data', function (req, res) {
+  res.header("Content-Type",'application/json');
+  res.send(parsedData);
+})
